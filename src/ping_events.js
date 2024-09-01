3eae5e3b2fc75922338dfbfc2bf2076a09e52f32
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2024-09-01 18:28:16 +0200

    add

--- src/ping_events.js
+++ src/ping_events.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
-import fetch from 'node-fetch';
+import schedule from 'node-schedule'
 import { Client, GatewayIntentBits, Partials } from "discord.js";
-import { event2discord } from "./event_transforms.js";
 
 dotenv.config({ path: ".env" });
 const { DISCORD_TOKEN, DISCORD_SERVER, CODEFORCES_CHANNEL, CODEFORCES_ROLE } = process.env;
@@ -13,7 +12,7 @@ const client = new Client({
 
 client.login(DISCORD_TOKEN);
 
-async function ping_events() {
+async function ping_upcoming() {
   const guild = await client.guilds.fetch(DISCORD_SERVER);
   const events = await guild.scheduledEvents.fetch();
   const role = "<@&" + CODEFORCES_ROLE + "> ";
@@ -25,6 +24,8 @@ async function ping_events() {
       .map(msg => msg.content.split(' ').find(word => word.includes("codeforces.com")));
   });
 
+  console.log();
+  console.log("Pinging events starting:");
   const now = new Date();
   console.log(now.toString());
   console.log();
@@ -45,9 +46,35 @@ async function ping_events() {
   }
 }
 
-client.once("ready", async () => {
-  console.log("Running:");
-  await ping_events();
-  setInterval(ping_events, 10 * 1000);
+async function ping_tomorrow() {
+  const guild = await client.guilds.fetch(DISCORD_SERVER);
+  const events = await guild.scheduledEvents.fetch();
+  const role = "<@&" + CODEFORCES_ROLE + "> ";
+  const channel = await guild.channels.fetch(CODEFORCES_CHANNEL);
+
   console.log();
+  console.log("Pinging events tomorrow:");
+  const now = new Date();
+  console.log(now.toString());
+  console.log();
+
+  for (const [id, event] of events) {
+    console.log(event.name)
+    console.log(event.scheduledStartAt.toString());
+
+    const url = event.entityMetadata.location;
+    if(!url.includes("codeforces.com")) continue;
+
+    const delta = event.scheduledStartAt.getTime() - now.getTime();
+    if(delta<0 || 3*24*60*60*1000<delta) continue;
+
+    const timestamp = "<t:"+event.scheduledStartTimestamp/1000+":t>";
+    channel.send(role + url + " at " + timestamp+" tomorrow.");
+  }
+}
+
+client.once("ready", async () => {
+  console.log("Client ready!")
+  setInterval(ping_upcoming, 10 * 1000);
+  schedule.scheduleJob('29 18 * * *', ping_tomorrow);
 });
