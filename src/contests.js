52f6a1b3f2e787440f2714161b7f5575a4c7e6ad
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2024-09-01 17:08:31 +0200

    add

--- src/contests.js
+++ src/contests.js
@@ -1,10 +1,16 @@
 import dotenv from "dotenv";
 import fetch from 'node-fetch';
 
-import { Client, GatewayIntentBits, Partials, GuildScheduledEventPrivacyLevel, GuildScheduledEventEntityType } from "discord.js";
+import {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  GuildScheduledEventPrivacyLevel,
+  GuildScheduledEventEntityType
+} from "discord.js";
 
 dotenv.config({ path: ".env" });
-const { DISCORD_TOKEN } = process.env;
+const { DISCORD_TOKEN, DISCORD_SERVER } = process.env;
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
@@ -13,43 +19,33 @@ const client = new Client({
 
 client.login(DISCORD_TOKEN);
 
-function contest2event(codeforces_contest)
-{
-  return {
-    name: codeforces_contest.name,
-    start: new Date(codeforces_contest.startTimeSeconds * 1000),
-    end: new Date((codeforces_contest.startTimeSeconds + codeforces_contest.durationSeconds) * 1000),
-    url: `https://codeforces.com/contests/${codeforces_contest.id}`
-  };
-}
-
-function event2discord(event)
-{
-  return {
-    name: event.name,
-    scheduledStartTime: event.start,
-    scheduledEndTime: event.end,
-    description: '',
-    entityMetadata: { location: event.url },
-    image: null,
-    reason: 'Codeforces contest',
-    entityType: GuildScheduledEventEntityType.External,
-    privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
-  };
-}
-
 async function get_contests() {
   const response = await fetch('https://codeforces.com/api/contest.list');
+  if (!response.ok) throw new Error("CF contest fetch request failed: " + response.status + ", " + response.statusText);
+
   const data = await response.json();
-  if (data.status !== 'OK') throw new Error('Failed to fetch contests from Codeforces\n' + comment);
+  if (data.status !== 'OK') throw new Error('CF contest fetch response not OK' + comment);
+
   return data.result
     .filter(contest => contest.phase === 'BEFORE')
     .map(contest2event);
 }
 
-async function add_contests_to_discord() {
-  const guild = await client.guilds.fetch("1029784287152705596");
-  const contests = await get_contests();
+async function update_discord_events() {
+  let contests = await get_contests();
+
+  const guild = await client.guilds.fetch(DISCORD_SERVER);
+  const events = await guild.scheduledEvents.fetch();
+
+  for (const [id, event] of events) {
+    const url = event.entityMetadata.location;
+
+    const contest = contests.filter(c => c.url === url)[0];
+    if (contest) guild.scheduledEvents.edit(event, event2discord(contest));
+
+    contests = contests.filter(c => c.url !== url);
+  }
+
   for (const contest of contests) {
     guild.scheduledEvents.create(event2discord(contest));
   }
@@ -57,24 +53,7 @@ async function add_contests_to_discord() {
 
 client.once("ready", async () => {
   console.log("Bot is ready!");
-  let contests = await get_contests();
-
-  const guild = await client.guilds.fetch("1029784287152705596");
-  const events = await guild.scheduledEvents.fetch();
-
-  for (const [id, event] of events)
-  {
-    const url = event.entityMetadata.location;
-    const matching = contests.filter(contest => contest.url === url).map(event2discord);
-    if(matching[0]) guild.scheduledEvents.edit(event, matching[0]);
-    contests = contests.filter(contest => contest.url !== url);
-  }
-
-  for (const contest of contests)
-  {
-    guild.scheduledEvents.create(event2discord(contest));
-  }
-
+  await update_discord_events();
   console.log("Done.");
 });
 
